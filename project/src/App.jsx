7f3c405b8c619c7f31a51951293fefb718c38958
harsh2329 <hsoni2392003@gmail.com>
2025-07-19 16:16:15 +0530

    further changes in dashboard routing

--- project/src/App.jsx
+++ project/src/App.jsx
@@ -23,7 +23,7 @@ function App() {
       <Routes>
         <Route path='/customers' element={<CustomerManagement />} />
         <Route path='/devices' element={ <DeviceModal/>} />
-        <Route path ='/dashboard' element ={<MainDashboard />} />
+        <Route path ='/' element ={<MainDashboard />} />
       </Routes>
     </div>
   );
